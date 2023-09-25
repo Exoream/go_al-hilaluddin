9ee337bbebf05bc2f42684ce4c346770ac8b8514
Al Hilaluddin <shiigetsu1@gmail.com>
2023-09-25 16:20:13 +0800

    update bcrypt

--- 18_Middleware/praktikum/routes/core_routes.go
+++ 18_Middleware/praktikum/routes/core_routes.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func New() *echo.Echo {
 	// create a new echo instance
 	e := echo.New()
+
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
 	// Route / to handler function
 	UserRoutes(e)
 	BookRoutes(e)
